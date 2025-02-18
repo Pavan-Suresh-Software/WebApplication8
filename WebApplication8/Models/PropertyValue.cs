608fdd7aa87a1f058ea6a71d03087ec4bebd937a
Pavan Suresh <pavansuresh.net@outlook.com>
2025-02-18 16:43:34 +0530

    add this

--- WebApplication8/Models/PropertyValue.cs
+++ WebApplication8/Models/PropertyValue.cs
@@ -1,13 +1,10 @@
 ﻿namespace WebApplication8.Models
 {
     public class PropertyValue:hidedata
-    {
-               
+    {               
         public int Id { get; set; }
         public string? PropertyName { get; set; }
         public int Value { get; set; }
-        public string labeltype { get; set; }
-        
-                
+        public string labeltype { get; set; }                        
     }
 }
